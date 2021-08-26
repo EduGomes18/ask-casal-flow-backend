ca68b6796f04dcbcf12e5ceb75b1493d9e4457d5
EduGomes18 <eduardo_gomes1318@outlook.com>
2021-08-25 22:05:15 -0300

    Finaliza configuracoes mongo db e comecar criar users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  name: string;
+  avatar: string;
+  email: string;
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schemas/users.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const createUser = new this.userModel(createUserDto);
+    return createUser.save();
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
